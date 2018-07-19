70a0cfc3ee3c93ec5810843a096fdb5d06783025
chiehan1 <chieh.an.hsu@gmail.com>
2018-07-19 13:05:14 +0800

    add pb tag

--- addPage.js
+++ addPage.js
@@ -4,11 +4,11 @@ var imageList = fs.readFileSync('./savedlist', 'utf8').split('\n');
 var ocrResult = fs.readFileSync('./OCR-result.txt', 'utf8');
 var pageCount = 0;
 
-imageList.push('');
 ocrResult = '[page_separator]<pb>' + ocrResult;
 
 ocrResult = ocrResult.replace(/\[page_separator\]<pb>/g, function(m) {
-  return imageList[pageCount++] + '\n\n';
+  var imageName = imageList[pageCount++];
+  return imageName ? '<pb id="' + imageName + '"/>\n' : '';
 });
 
 fs.writeFileSync('./OCR-result.txt', ocrResult, 'utf8');
